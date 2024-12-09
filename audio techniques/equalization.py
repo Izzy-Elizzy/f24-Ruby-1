548952454a6e37416c47b85f3b28e83870e132c9
Dima <dmitrybochkarrev@gmail.com>
2024-12-09 14:46:13 -0500

    minor fix

--- audio techniques/equalization.py
+++ audio techniques/equalization.py
@@ -10,16 +10,3 @@ def apply_equalization(audio_data, sample_rate, freq_center=1000, gain=5, q=0.7)
     sos = butter(2, [freq_center_norm - bandwidth/2, freq_center_norm + bandwidth/2], 'bandpass', output='sos')
     equalized_audio = sosfilt(sos, audio_data) * (10**(gain/20))
     return equalized_audio
-
-    #noise_reduction.py 
-    import librosa
-    import numpy as np
- 
-def apply_noise_reduction(audio_data, sample_rate, n_fft=2048, hop_length=512):
-    """Applies noise reduction using spectral subtraction."""
-    stft = librosa.stft(audio_data, n_fft=n_fft, hop_length=hop_length)
-    noise_mag = np.mean(np.abs(stft[:, :10]), axis=1, keepdims=True)
-    mask = np.maximum(1 - (noise_mag / np.abs(stft)), 0)
-    masked_stft = stft * mask
-    reduced_noise_audio = librosa.istft(masked_stft, hop_length=hop_length)
-    return reduced_noise_audio
